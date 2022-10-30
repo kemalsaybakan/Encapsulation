b9f4d507f4ae6d7045a8eb86995c777b9daa1bc9
Kemal Saybakan <kemalsaybakan@gmail.com>
2022-10-30 14:28:42 +0300

    First commit

---
+++ Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Book
+    {
+        // Class variables
+        private int pageNumber;
+        private String bookName, author;
+        
+        // Constructor Method
+        public Book(String bookName, int pageNumber, String author)
+        {
+            this.bookName = bookName;
+            this.pageNumber = pageNumber;
+            this.author = author;
+
+            if (pageNumber < 1)
+            {
+                this.pageNumber = 1;
+            }
+            else
+            {
+                this.pageNumber = pageNumber;
+            }
+        }
+
+        // Getter and Setter Methods
+        public int getPageNumber()
+        {
+            return this.pageNumber;
+        }
+
+        public void setPageNumber(int pageNumber)
+        {
+            if (pageNumber < 1)
+            {
+                this.pageNumber = 1;
+            }
+            else
+            {
+                this.pageNumber = pageNumber;
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Encapsulation;
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Book Object
+            Book bookObj = new Book("Beginner to OOP Programming with C#", -10, "Kemal Saybakan");
+            Console.WriteLine(bookObj.getPageNumber());
+
+            // Change Book Class - Page Number Variable
+            bookObj.setPageNumber(50);
+            Console.WriteLine(bookObj.getPageNumber());
+        }
+    }
+}
